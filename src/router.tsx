5715a4c729e6b1e82fb14cdefdc13ad41b905117
chsua <csj1919@naver.com>
2023-05-11 21:37:11 +0900

    temp

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
 import ProductListPage from '@Pages/ProductListPage';
 
 import App from './App';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
